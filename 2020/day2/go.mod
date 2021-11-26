297f1b15b7e76f178ae352d88ce8d0196fab5257
thisisdavidbell <thisisdavidbell@hotmail.com>
2021-11-26 22:45:40 +0000

    quick and dirty solution for day2

---
+++ 2020/day2/go.mod
@@ -0,0 +1,3 @@
+module github.com/thisisdavidbell/adventofcode/2020/day2
+
+go 1.13
---
+++ 2020/day2/passwordcheck.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	count, count2 := solveItForFile("test-input.txt")
+	fmt.Printf("Part 1 test count: %v\n", count)
+	fmt.Printf("Part 2 test count: %v\n\n", count2)
+
+	countReal, countReal2 := solveItForFile("real-input.txt")
+	fmt.Printf("Part 1 real count: %v\n", countReal)
+	fmt.Printf("Part 2 real count: %v\n", countReal2)
+
+}
+
+// PWData - struct for key values from each line of file
+type PWData struct {
+	min      int
+	max      int
+	letter   string
+	password string
+}
+
+// checkError -  check for errors
+func checkErr(name string, err error) {
+	if err != nil {
+		fmt.Printf("Error found at %v. error: %v", name, err)
+		os.Exit(1)
+	}
+}
+
+// ImportFileToSlice - read each line into a slice
+func ImportFileToSlice(filename string) (lineSlice []string, err error) {
+	f, err := os.Open(filename)
+	defer f.Close()
+	checkErr("Open", err)
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		lineSlice = append(lineSlice, scanner.Text())
+	}
+	return lineSlice, err
+}
+
+// SliceLinesToSliceStruct convert slice of lines to slice of structs
+func SliceLinesToSliceStruct(lineSlice []string) (pwSlice []PWData, err error) {
+	//ToDo: more efficient way, probably fmt.Sscanf
+	for _, line := range lineSlice {
+		myregex := regexp.MustCompile("[-: ]+")
+		theSlice := myregex.Split(line, -1)
+		minVal, _ := strconv.Atoi(theSlice[0])
+		maxVal, _ := strconv.Atoi(theSlice[1])
+		pwData := PWData{
+			min:      minVal,
+			max:      maxVal,
+			letter:   theSlice[2],
+			password: theSlice[3],
+		}
+		pwSlice = append(pwSlice, pwData)
+	}
+	return
+}
+
+func checkValidPasswordPart1(pwData PWData) bool {
+	count := strings.Count(pwData.password, pwData.letter)
+	return count >= pwData.min && count <= pwData.max
+}
+
+func checkValidPasswordPart2(pwData PWData) bool {
+	match1 := string(pwData.password[pwData.min-1]) == pwData.letter
+	match2 := string(pwData.password[pwData.max-1]) == pwData.letter
+
+	return (match1 && !match2) || (!match1 && match2)
+}
+
+func countValidPWsInPWDataPart1(pwDataSlice []PWData) (count int) {
+	for _, pwData := range pwDataSlice {
+		if checkValidPasswordPart1(pwData) {
+			count++
+		}
+	}
+	return
+}
+
+func countValidPWsInPWDataPart2(pwDataSlice []PWData) (count int) {
+	for _, pwData := range pwDataSlice {
+		if checkValidPasswordPart2(pwData) {
+			count++
+		}
+	}
+	return
+}
+
+func solveItForFile(inputfile string) (count int, count2 int) {
+	lineSlice, _ := ImportFileToSlice(inputfile)
+	structSlice, _ := SliceLinesToSliceStruct(lineSlice)
+	count = countValidPWsInPWDataPart1(structSlice)
+	count2 = countValidPWsInPWDataPart2(structSlice)
+	return
+}
